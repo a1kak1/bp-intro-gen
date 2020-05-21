cd56193afa35b35348e6daeb760c42d6b8fd746f
a1kak1 <23013530+a1kak1@users.noreply.github.com>
2020-05-22 07:43:34 +0900

    部分的にスライダーに対応
    クリック、矢印キーでの選択のみ実装
    ドラッグや複数選択は未実装

--- gen.js
+++ gen.js
@@ -67,17 +67,30 @@ function addEnterClickTo(el) {
   });
 }
 
+let isOnMouseLeftDown = false;
+const MOUSE_LEFT = 0;
+document.addEventListener('mousedown', ({ button }) => {
+  if (button === MOUSE_LEFT) {
+    isOnMouseLeftDown = true;
+  }
+}, true);
+document.addEventListener('mouseup', ({ button }) => {
+  if (button === MOUSE_LEFT) {
+    isOnMouseLeftDown = false;
+  }
+}, true);
+
 let isOnTabDown = false;
 document.addEventListener('keydown', ({ key }) => {
   if (key === 'Tab') {
     isOnTabDown = true;
   }
-});
+}, true);
 document.addEventListener('keyup', ({ key }) => {
   if (key === 'Tab') {
     isOnTabDown = false;
   }
-});
+}, true);
 
 function addTabFocusTo(el) {
   el.addEventListener('focus', () => {
@@ -224,5 +237,66 @@ export function enableUserRadiobutton(query, appearanceQuery) {
       addEnterClickTo(radiobutton);
       addTabFocusTo(radiobutton);
     }
+  }
+}
+
+const arrowOffsetMap = {
+  'ArrowLeft': -1,
+  'ArrowRight': 1
+};
+
+function clamp(value, min, max) {
+  return Math.min(Math.max(value, min), max);
+}
+
+function renderSlider(slider, scales, appearance) {
+  const scale = scales.find(el => el.dataset['value'] === slider.dataset['value']);
+  appearance.style.setProperty('left', scale.dataset['offset']);
+  appearance.style.setProperty('width', appearance.dataset['basicWidth']);
+  emitImageGenerationBy(slider);
+}
+
+/**
+ * @param {string} query
+ * @param {string} scaleQuery
+ * @param {string} appearanceQuery
+ */
+export function enableUserSlider(query, scaleQuery, appearanceQuery) {
+  let sliderGroupMap = {};
+  for (const slider of document.querySelectorAll(query)) {
+    sliderGroupMap[slider.dataset['group']] = {
+      slider,
+      scales: []
+    };
+  }
+  for (const scale of document.querySelectorAll(scaleQuery)) {
+    sliderGroupMap[scale.dataset['group']].scales.push(scale);
+  }
+  for (const appearance of document.querySelectorAll(appearanceQuery)) {
+    sliderGroupMap[appearance.dataset['group']].appearance = appearance;
+  }
+
+  for (const { slider, scales, appearance } of Object.values(sliderGroupMap)) {
+    slider.addEventListener('mousedown', ev => {
+      if (!isOnMouseLeftDown) {
+        return;
+      }
+      const scale = ev.target;
+      slider.dataset['value'] = scale.dataset['value'];
+      renderSlider(slider, scales, appearance);
+    });
+    slider.addEventListener('keydown', ({ key }) => {
+      if (key !== 'ArrowLeft' && key !== 'ArrowRight') {
+        return;
+      }
+      if (!slider.dataset['value']) {
+        slider.dataset['value'] = scales.length - 1 >> 1;
+      } else {
+        const value = Number(slider.dataset['value']) + arrowOffsetMap[key];
+        slider.dataset['value'] = clamp(value, 0, scales.length - 1);
+      }
+      renderSlider(slider, scales, appearance);
+    });
+    addTabFocusTo(slider);
   }
 }
